09945df694043f79620987f07907913409ab42e3
Ray-dev28 <74359684+Ray-dev28@users.noreply.github.com>
2021-01-07 13:22:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var days = 0;
+function addDays(){
+    days++
+    if(days==366){
+        window.close("2020");
+        alert("goodbye 2020");
+    }
+        else(){
+            window.open("open 2021");
+            alert("HAPPY NEW YEAR");
+        }
+       
+    
+    
+}
